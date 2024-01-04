e3bef101c6de8d15fcceeecb05e0a0e4fe24e439
promorshed45 <ctgmorshed45@gmail.com>
2024-01-04 09:49:58 +0600

    update

---
+++ shop.js
@@ -0,0 +1,16 @@
+function selectCardItem(){
+    const selectItemTitle = document.getElementById('item-title');
+    const selectItemPrice = document.getElementById('item-price')
+    
+    
+    
+    console.log(selectItemTitle?.innerText);
+    console.log(selectItemPrice?.innerText);
+    
+    
+}
+
+
+function link(){
+    location.href = 'index.html';
+}
