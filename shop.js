77b316c6d91986d540e6dee35a4b589b13ac6c86
promorshed45 <ctgmorshed45@gmail.com>
2024-01-09 09:16:19 +0600

    update dom

--- shop.js
+++ shop.js
@@ -1,12 +1,22 @@
-function selectCardItem(){
-    const selectItemTitle = document.getElementById('item-title');
-    const selectItemPrice = document.getElementById('item-price')
+function selectCardItem(target){
+    const itemTitle = target.querySelector('.item-title').innerText;
     
+
+    const setItemList = document.getElementById('item-list');
+    const li = document.createElement('li');
+    li.innerText = itemTitle;
+    setItemList.appendChild(li);
+
+    const itemPrice = parseInt(target.querySelector('.item-price').innerText);
+    const setTotalPrice = document.getElementById("total-price");
+    setTotalPrice.innerText = itemPrice;
+
     
+    const setGrandTotal = document.getElementById("grand-total");
+    setGrandTotal.innerText = itemPrice;
+
     
-    console.log(selectItemTitle?.innerText);
-    console.log(selectItemPrice?.innerText);
-    
+
     
 }
 
